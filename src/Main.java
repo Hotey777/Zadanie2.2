f6f16312b5963211d86c1b85240b9986a4355c53
Hotey777 <volksuper1@gmail.com>
2022-07-12 13:20:37 +0300

    Zadanie2.2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 100; //Начальный счёт
-        int y = 1000; //Сумма пополнения
-        int l = 100; //1 рубль за 100 рублей
-        int z = x + y; //Общая суммая при пополнении
-        int bonus, o;
+        int amount = 100; //Начальный счёт
+        int deposit = 1000; //Сумма пополнения
+        int cashback = 100; //1 рубль за 100 рублей
+        int sum = amount + deposit; //Общая сумма при пополнении
+        int bonus, result;
 
-        if (y >= 1000) {
-            bonus = y / l;
+        if (deposit >= 1000) {
+            bonus = deposit / cashback;
             System.out.println(bonus + " Бонусных рублей");
         } else {
             bonus = 0;
             System.out.println("Вы не получили бонус");
         }
 
-        o = z + bonus; //Итоговая сумма с бонусом
-        System.out.println(o + " Итоговый счет");
+        result = sum + bonus; //Итоговая сумма с бонусом
+        System.out.println(result + " Итоговый счет");
     }
 }
